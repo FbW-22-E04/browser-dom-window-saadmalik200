aabb10b3bd47061ac95c09841699943da1facc5b
Saad Malik <saad_malik200@hotmail.com>
2022-09-15 15:04:24 +0200

    ex1 final

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const randomNum = String(Math.floor(Math.random() * 10 + 1));
-
+console.log(randomNum);
 let counter = 0;
 
 const input = prompt("Guess a number between 1-10:");
-// console.log(input);
 
 for (let i = 0; i < 2; i++) {
   if (input === randomNum) {
     counter++;
-    alert(`Success, the number was ${randomNum}! Attempts: ${counter} `);
+    alert(`Success, the number was ${randomNum} ! Attempts: ${counter} `);
     break;
   } else {
     counter++;
-    // console.log("Counter is:", counter);
+    console.log("Counter is:", counter);
     const input2 = prompt("Wrong!!! Try Again!");
-    // console.log("input2 is: ", input2);
+    console.log("input2 is: ", input2);
     if (input2 === randomNum && counter !== 3) {
       counter++;
-      alert(`Success, the number was ${randomNum}! Attempts: ${counter} `);
+      alert(`Success, the number was ${randomNum} ! Attempts: ${counter} `);
       break;
     } else if (input2 !== randomNum && counter === 2) {
       alert(
